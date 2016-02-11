ad7a661a82ec9671f6603d027e67a65259cc299a
Marcle Rodrigues <maarclee@gmail.com>
2016-02-11 20:24:09 -0300

    Create ex7.rb

---
+++ ex7.rb
@@ -0,0 +1,22 @@
+puts "Mary had a little lamb." # Just a string
+puts "Its fleece was white as #{'snow'}."# Just a string with a string ;D
+puts "And everywhere that Mary went."# Another string
+puts "." * 10 # Print 10 times the dot
+
+end1 = "C" # Define end1 equals C
+end2 = "h" # Define end2 equals h
+end3 = "e" # Define end3 equals e
+end4 = "e" # Define end4 equals e
+end5 = "s" # Define end5 equals s
+end6 = "e" # Define end6 equals e
+end7 = "B" # Define end7 equals B
+end8 = "u" # Define end8 equals u
+end9 = "r" # Define end8 equals r
+end10 = "g" # Define end10 equals g
+end11 = "e" # Define end11 equals e
+end12 = "r" # Define end12 equals r
+
+# Show a string with the value Cheese without a new line after
+print end1 + end2 + end3 + end4 + end5 + end6
+# Show a string with the value Burger with a new line after
+puts end7 + end8 + end9 + end10 + end11 + end12
