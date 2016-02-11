8dcc1ad6f065152c58b10a54d686b345e12d1f36
Marcle Rodrigues <maarclee@gmail.com>
2016-02-10 23:08:33 -0300

    Create ex5.rb

---
+++ ex5.rb
@@ -0,0 +1,30 @@
+# Define the name
+my_name = 'Zed. A. Shaw'
+# Define the age
+my_age = 35 # not a lie in 2009
+# Define the height
+my_height = 74 # inches
+# Define the weigth
+my_weight = 180 # lbs
+# Define the color of eyes
+my_eyes = 'Blue'
+# Define the color of the teeth
+my_teeth = 'White'
+# Define the color of the hair
+my_hair = 'Brown'
+
+# Show about who we are talking of.
+puts "Let's talk about #{my_name}."
+# Show he's heigth
+puts "He's #{my_height} inches tall."
+# Show he's weigth
+puts "He's #{my_weight} pounds heavy."
+# Show that him it's not too heavy
+puts "Actually that's not too heavy."
+# Show the color of the eyes and the hair.
+puts "He's got #{my_eyes} and #{my_hair} hair."
+# Show the color of the teeth
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try yo get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_weight}."
