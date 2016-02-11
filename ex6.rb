54024602fec162589d776b4e9d2c9ca506b4942f
Marcle Rodrigues <maarclee@gmail.com>
2016-02-10 23:15:34 -0300

    Create ex6.rb

---
+++ ex6.rb
@@ -0,0 +1,36 @@
+# Define the number of people
+types_of_people = 10
+# Define x as a string
+x = "There are #{types_of_people} type of people."
+# Define binary
+binary = "binary"
+# Define don't
+do_not = "don't"
+# Define y
+y = "Those who know #{binary} and those who #{do_not}"
+
+# Show the string x
+puts x
+# Show the string y
+puts y
+
+# Show x embed
+puts "I said: #{x}."
+# Show y embed
+puts "I also said: '#{y}'."
+
+# Define a boolean value for hilarious
+hilarious = false
+# Define joke evaliation
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# Shows joke evaluation
+puts joke_evaluation
+
+# Define w
+w = "This is the left side of..."
+# Define e
+e = "a string with a right side."
+
+# Show a string formatted with w plus e
+puts w + e
