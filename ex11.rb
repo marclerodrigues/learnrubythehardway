76571bc64e4f05bf12c9a77cc16323a984792dfe
Marcle Rodrigues <maarclee@gmail.com>
2016-02-11 21:13:55 -0300

    Create ex11.rb

---
+++ ex11.rb
@@ -0,0 +1,13 @@
+print "How old are you?"
+age = gets.chomp
+print "How tal are you?"
+height = gets.chomp
+print "How much do you weight?"
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+print "Where do you live?"
+city = gets.chomp
+
+puts "So, you live in #{city}."
