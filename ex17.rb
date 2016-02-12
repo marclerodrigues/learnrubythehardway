1197b588bb450e2acb3ae8ede82a6232617c68ee
Marcle Rodrigues <maarclee@gmail.com>
2016-02-11 22:32:39 -0300

    Create ex17.rb

---
+++ ex17.rb
@@ -0,0 +1,13 @@
+from_file, to_file = ARGV
+
+puts "Copying from #{from_file} to #{to_file}"
+
+in_file = open(from_file).read
+
+puts "The input is #{in_file.length} bytes long"
+
+out_file = open(to_file, 'w')
+out_file.write(in_file)
+
+puts "Alright, all done."
+out_file.close
